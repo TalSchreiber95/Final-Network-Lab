ef7d8e2011ce130375a373dc4fcb4b4b0316bd1a
OmerShalom <73235082+Omer2041@users.noreply.github.com>
2021-03-04 16:03:07 +0200

    add c files

---
+++ C part/Task 2.1B Writing Filters/sniffB1.c
@@ -0,0 +1,79 @@
+#include <pcap/pcap.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+
+// Ethernet header
+struct ethheader
+{
+    u_char ether_dhost[6]; // destination host address
+    u_char ether_shost[6]; // source host address
+    u_short ether_type;    // protocol type (IP, ARP, RARP, etc)
+};
+
+// IP Header
+struct ipheader
+{
+    unsigned char iph_ihl : 4,       // IP header length
+        iph_ver : 4;                 // IP version
+    unsigned char iph_tos;           // Type of service
+    unsigned short int iph_len;      // IP Packet length (data + header)
+    unsigned short int iph_ident;    // Identification
+    unsigned short int iph_flag : 3, // Fragmentation flags
+        iph_offset : 13;             // Flags offset
+    unsigned char iph_ttl;           // Time to Live
+    unsigned char iph_protocol;      // Protocol type
+    unsigned short int iph_chksum;   // IP datagram checksum
+    struct in_addr iph_sourceip;     // Source IP address
+    struct in_addr iph_destip;       // Destination IP address
+};
+
+void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet) {
+    struct ethheader *eth = (struct ethheader *)packet;
+
+    if (ntohs(eth->ether_type) == 0x0800) { // IP type is 0x0800
+        struct ipheader *ip = (struct ipheader *)(packet + sizeof(struct ethheader));
+
+        printf("Pkt Source: %s\n", inet_ntoa(ip->iph_sourceip));
+        printf("Pkt Destination: %s\n", inet_ntoa(ip->iph_destip));
+        printf("\n");
+
+        // print by protocol
+        if (ip->iph_protocol == IPPROTO_TCP) {
+            printf("Protocol: TCP\n");
+            return;
+        } else if (ip->iph_protocol == IPPROTO_UDP){
+            printf("Protocol: UDP\n");
+            return;
+        } else if (ip->iph_protocol == IPPROTO_ICMP){
+            printf("Protocol: ICMP\n");
+            return;
+        } else {
+            printf("Protocol: others\n");
+            return;
+        }
+    }
+}
+
+int main() {
+    pcap_t *handle;
+    char error[PCAP_ERRBUF_SIZE];
+    bpf_u_int32 net;
+    struct bpf_program fp;
+    char filter_exp [] = "icmp and src host 10.0.2.4 and dst host 10.0.9.1";
+
+
+    // Step 1: Open live pcap session on the device: "enp0s3"
+    handle = pcap_open_live("enp0s3", BUFSIZ, 1, 1000, error);
+
+    // Step 2: Compile the filter and set it
+    if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) printf("failed compiling filter");
+    pcap_setfilter(handle, &fp);
+
+    // Step 3: Capture packets
+    pcap_loop(handle, -1, got_packet, NULL);
+
+
+    // Step 4: Close the handle
+    pcap_close(handle); 
+    return 0;
+}
